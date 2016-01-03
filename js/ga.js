74005ef79dc9d2138599c48b4786490043ac3ad7
Damien Octeau <octeau@cse.psu.edu>
2016-01-03 01:33:52 -0500

    Updated publications.

--- js/ga.js
+++ js/ga.js
@@ -3,5 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-59412354-1', 'auto');
+ga('create', 'UA-59412354-2', 'auto');
 ga('send', 'pageview');
